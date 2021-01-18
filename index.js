3ef30b827d588e6e5fdc3d65f7e4c8f6e0aa0977
Aravind Venkatesan <aravindtherocker@gmail.com>
2021-01-18 18:15:25 +0800

    part3_ex_3.8_commit

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan((tokens, req, res) => {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'), '-',
+      'BODY Data: ', JSON.stringify(req.body),
+      tokens['response-time'](req, res), 'ms'
+    ].join(' ')
+  }))
 const PORT = 3001
 
 let persons = [
