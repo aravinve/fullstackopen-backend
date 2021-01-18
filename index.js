30d8797b403ef85c6740443dc9899ae5ce72778c
Aravind Venkatesan <aravindtherocker@gmail.com>
2021-01-18 18:34:51 +0800

    Heroku DeployCommit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(morgan((tokens, req, res) => {
       tokens['response-time'](req, res), 'ms'
     ].join(' ')
   }))
-const PORT = 3001
+
 
 let persons = [
     {
@@ -79,6 +79,8 @@ app.delete('/api/persons/:id', (req, res) => {
     persons = persons.filter(person => person.id !== paramId)
     res.status(204).end()
 })
+
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`Server Running on port ${PORT}`)
